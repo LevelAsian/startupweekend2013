12812502a9735245df4bb6a2e3e45e31002c5a56
jQrgen <jorgen.notland@gmail.com>
2013-10-12 19:18:47 +0200

    småkobling

--- Server/app/js/controllers.js
+++ Server/app/js/controllers.js
@@ -6,9 +6,18 @@ angular.module('myApp.controllers', [])
     .controller('mainCtrl', function($scope, $http, $location, $rootScope, $route) {
     $scope.hei = "basdbasdbdadsa";
 
+    $scope.goToPassenger_input = function(){
+        $location.path("/passenger_input");
+    }
+
+    $scope.goToDriver_input = function(){
+        $location.path("/driver_input");
+    }
+
     })
 
     .controller('driver_inputCtrl', function($scope, $routeParams, $http, $rootScope, $route, $location) {
+
 
     })
 
