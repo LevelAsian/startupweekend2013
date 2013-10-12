922ca18dc2999dbf3baa962f6172c6b2195134fa
Ton Mangmee <ton.mangmee@gmail.com>
2013-10-12 16:15:39 +0200

    register

--- Server/app/js/controllers/register.js
+++ Server/app/js/controllers/register.js
@@ -3,9 +3,22 @@
 var hasreloaded = false;
 
 myApp.controller('registerCtrl', function($scope, $http, $location, $rootScope, route) {
+    $scope.register = function(customerFirstName, customerLastName, customerEmail,
+                               customerPassword, rewritePassword, customerPhoneNumber,
+                               creditCardNumber, cvv, expirationDate, postalCode ) {
+        customer.firstname = customerFirstName;
+        customer.lastname = customerLastName;
+        customer.email = customerEmail;
+        customer.password = customerPassword;
+        customer.phone = customerPhoneNumber;
+        customer.bankInfo.creditcardnumber = creditCardNumber;
+        customer.bankInfo.cvv = cvv;
+        customer.bankInfo.expirationdate = expirationDate;
+        customer.bankInfo.postalcode = postalCode;
 
+        $location.url('/');
+    };
 });
-
 
 
 
