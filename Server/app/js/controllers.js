7027156fb7ce5a45d4b898956d35e29227da2fde
Ton Mangmee <ton.mangmee@gmail.com>
2013-10-13 12:26:50 +0200

    css register

--- Server/app/js/controllers.js
+++ Server/app/js/controllers.js
@@ -27,3 +27,25 @@ angular.module('myApp.controllers', [])
     .controller('registerCtrl', function($scope, $http, $location, $rootScope){
 
     })
+
+    .controller('registerCtrl', function($scope, $http, $location, $rootScope, route) {
+        $scope.register = function(customerFirstName, customerLastName, customerEmail,
+                                   customerPassword, rewritePassword, customerPhoneNumber,
+                                   creditCardNumber, cvv, expirationDate, postalCode ) {
+            customer.firstname = customerFirstName;
+            customer.lastname = customerLastName;
+            customer.email = customerEmail;
+            customer.password = customerPassword;
+            customer.phone = customerPhoneNumber;
+            customer.bankInfo.creditcardnumber = creditCardNumber;
+            customer.bankInfo.cvv = cvv;
+            customer.bankInfo.expirationdate = expirationDate;
+            customer.bankInfo.postalcode = postalCode;
+
+            $location.path('/');
+
+
+        };
+    });
+
+
