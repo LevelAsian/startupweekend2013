0071176b4c8b230cd463891d5a183e210637638e
jQrgen <jorgen.notland@gmail.com>
2013-10-17 12:14:48 +0200

    vetdafaen

--- Server/app/js/controllers.js
+++ Server/app/js/controllers.js
@@ -18,6 +18,50 @@ angular.module('myApp.controllers', [])
 
     .controller('driver_inputCtrl', function($scope, $routeParams, $http, $rootScope, $route, $location) {
 
+    $scope.friends = [
+        {name:'John', age:25, gender:'boy'},
+        {name:'Jessie', age:30, gender:'girl'},
+        {name:'Johanna', age:28, gender:'girl'},
+        {name:'Joy', age:15, gender:'girl'},
+        {name:'Mary', age:28, gender:'girl'},
+        {name:'Peter', age:95, gender:'boy'},
+        {name:'Sebastian', age:50, gender:'boy'},
+        {name:'Erika', age:27, gender:'girl'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Patrick', age:40, gender:'boy'},
+        {name:'Samantha', age:60, gender:'girl'}
+    ]
+
+    $scope.currentclass = "driver_inputtomorrowbutton";
+    $scope.currentclass2 = "driver_inputtodaybutton";
+
+    $scope.changeclass1 = function(){
+        if($scope.currentclass == "driver_inputtomorrowbutton"){
+            $scope.currentclass = "driver_inputtomorrowbutton2";
+        }else{
+            $scope.currentclass = "driver_inputtomorrowbutton";
+        }
+    }
+
+    $scope.changeclass2 = function(){
+        if($scope.currentclass2 == "driver_inputtodaybutton"){
+            $scope.currentclass2 = "driver_inputtodaybutton2";
+        }else{
+            $scope.currentclass2 = "driver_inputtodaybutton";
+        }
+    }
+
 
     })
 
